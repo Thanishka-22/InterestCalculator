e88732d112a48b2221729dc5b2c39eb0878fd75b
Thanishka-22 <thanishkathirupathi@gmail.com>
2025-09-18 14:18:09 +0530

    Update MainClass.java
    Update MainClass to allow 5 user input iterations

--- Placement/src/com/wipro/bank/main/MainClass.java
+++ Placement/src/com/wipro/bank/main/MainClass.java
@@ -1,20 +1,33 @@
 package com.wipro.bank.main;
+
 import com.wipro.bank.service.BankService;
 import java.util.Scanner;
-public interface MainClass {
-    public static void main(String[] args)
-    {
-    	Scanner sc=new Scanner(System.in);
-    	System.out.print("Enter Tenure(in years):");
-    	int tenure=sc.nextInt();
-    	System.out.print("Enter Principal:");
-    	float principal=sc.nextFloat();
-    	System.out.print("Enter Age:");
-    	int age=sc.nextInt();
-    	System.out.print("Enter Gender:");
-    	String gender=sc.next();
-    	BankService service=new BankService();
-    	service.calculate(principal, tenure, age, gender);
-    	sc.close();
+
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+       
+        System.out.print("Enter number of test cases: ");
+        int t = sc.nextInt();
+        for (int i = 1; i <= t; i++) {
+            System.out.println("\n Test Case " + i +  " ");
+            System.out.print("Enter Tenure (in years): ");
+            int tenure = sc.nextInt();
+
+            System.out.print("Enter Principal: ");
+            float principal = sc.nextFloat();
+
+            System.out.print("Enter Age: ");
+            int age = sc.nextInt();
+
+            System.out.print("Enter Gender: ");
+            String gender = sc.next();
+
+            BankService service = new BankService();
+            service.calculate(principal, tenure, age, gender);
+        }
+
+        sc.close();
     }
 }
